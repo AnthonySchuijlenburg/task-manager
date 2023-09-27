3320d247b59afb66f355a01a2b8f86e87c7b0f25
Anthony Schuijlenburg <aschuijlenburg@swis.nl>
2023-09-27 22:08:14 +0200

    feat: add persist store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <script setup lang="ts">
 import NewTask from '@/components/NewTask.vue'
 import TaskBoard from '@/components/TaskBoard.vue'
+import { useTasksStore } from '@/stores/tasks'
+import { onBeforeMount } from 'vue'
+import { TaskStatus } from '@/enums/TaskStatus'
+
+const tasks = useTasksStore()
+
+onBeforeMount(() => {
+  if (!tasks.tasks.length) {
+    tasks.addTask({
+      id: tasks.latestTaskId + 1,
+      name: 'Wake up!',
+      description:
+        "This is the first and most critical task of the day. Don't get off on the wrong foot by snoozing multiple times, but just go for it!",
+      dueDate: new Date(),
+      status: TaskStatus.Done
+    })
+    tasks.addTask({
+      id: tasks.latestTaskId + 1,
+      name: 'Have a coffee',
+      description: 'This is what gets you going for the day, a nice warm cup of coffee.',
+      dueDate: new Date(),
+      status: TaskStatus.InProgress
+    })
+    tasks.addTask({
+      id: tasks.latestTaskId + 1,
+      name: 'Go for a run',
+      description:
+        'Getting in your daily exercise is important, it keeps your body and mind healthy.',
+      dueDate: new Date(),
+      status: TaskStatus.Todo
+    })
+  }
+})
 </script>
 
 <template>
--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -2,34 +2,40 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { Task } from '@/types/Task'
 
-export const useTasksStore = defineStore('tasks', () => {
-  const tasks = ref<Array<Task>>([])
-  // Start of at -1 so the first entry has an id of 0
-  const latestTaskId = ref<number>(-1)
+export const useTasksStore = defineStore(
+  'tasks',
+  () => {
+    const tasks = ref<Array<Task>>([])
+    // Start of at -1 so the first entry has an id of 0
+    const latestTaskId = ref<number>(-1)
 
-  function addTask(task: Task) {
-    const index: number = tasks.value.findIndex(
-      (taskToCheck): boolean => taskToCheck.id === task.id
-    )
-    if (index !== -1) {
-      throw new Error('A task with this Id already exists')
+    function addTask(task: Task) {
+      const index: number = tasks.value.findIndex(
+        (taskToCheck): boolean => taskToCheck.id === task.id
+      )
+      if (index !== -1) {
+        throw new Error('A task with this Id already exists')
+      }
+
+      tasks.value.push(task)
+      latestTaskId.value++
     }
 
-    tasks.value.push(task)
-    latestTaskId.value++
-  }
+    function removeTask(id: number) {
+      tasks.value = tasks.value.filter((task: Task): boolean => task.id !== id)
+    }
 
-  function removeTask(id: number) {
-    tasks.value = tasks.value.filter((task: Task): boolean => task.id !== id)
-  }
+    function updateTask(updatedTask: Task) {
+      const taskIndex: number = tasks.value.findIndex(
+        (task: Task): boolean => task.id === updatedTask.id
+      )
+      // let updatedTasks: Array<Task> = [...tasks.value];
+      tasks.value[taskIndex] = updatedTask
+    }
 
-  function updateTask(updatedTask: Task) {
-    const taskIndex: number = tasks.value.findIndex(
-      (task: Task): boolean => task.id === updatedTask.id
-    )
-    // let updatedTasks: Array<Task> = [...tasks.value];
-    tasks.value[taskIndex] = updatedTask
+    return { tasks, latestTaskId, addTask, removeTask, updateTask }
+  },
+  {
+    persist: true
   }
-
-  return { tasks, latestTaskId, addTask, removeTask, updateTask }
-})
+)
